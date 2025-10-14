ebe889b3aed60b78bd12d7b9b4e0c4d680271e28
avinashlingaloo <avinash@plusnarrative.com>
2025-10-14 04:50:02 +0400

    fix(messages): updated message service and controller

--- messages/src/messages/messages.controller.ts
+++ messages/src/messages/messages.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { Body, Controller, Get, NotFoundException, Param, Post } from '@nestjs/common';
 import { CreateMessageDto } from './dtos/create-message.dto';
 import { MessagesService } from './messages.service';
 
@@ -23,8 +23,13 @@ export class MessagesController {
   }
 
   @Get('/:id')
-  getMessage(@Param('id') id: string) {
+ async getMessage(@Param('id') id: string) {
     console.log("id received", id);
-    return this.messagesService.findOne(id);
+    const message = await this.messagesService.findOne(id);
+    if (!message) {
+      throw new NotFoundException('Message not found');
+    }
+    console.log("message received", message);
+    return message;
   }
 }
